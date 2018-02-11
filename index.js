731c03c71f6774cd5a4b6288d365adf8b14d43a0
Tiffany Tougard <snooopygirl@gmail.com>
2018-02-11 15:41:57 +0100

    Setup Finish

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-const express = require('express')
-const app = express()
-app.use(express.static('public'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+var express = require('express'),
+    path = require('path'),
+    fs = require('fs');
+
+var app = express();
+var staticRoot = __dirname + '/';
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(staticRoot));
+
+app.use(function(req, res, next){
+
+    // if the request is not html then move along
+    var accept = req.accepts('html', 'json', 'xml');
+    if(accept !== 'html'){
+        return next();
+    }
+
+    // if the request has a '.' assume that it's for a file, move along
+    var ext = path.extname(req.path);
+    if (ext !== ''){
+        return next();
+    }
+
+    fs.createReadStream(staticRoot + './dist/index.html').pipe(res);
+
+}); 
+
+app.listen(app.get('port'), function() {
+    console.log('app running on port', app.get('port'));
+});
