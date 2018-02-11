90a68b36b9cdbc34761e3284dcb403e4f100160a
Tiffany Tougard <snooopygirl@gmail.com>
2018-02-11 15:43:19 +0100

    Setup Finish

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'tiffanytougard',
-    script: './src/main.js'
+    script: './index.js'
   }],
   deploy: {
     production: {
@@ -11,7 +11,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:TiffanyTougard/repo.git',
       path: '/home/bitnami/repo',
-      'post-deploy': 'npm install && npm run nodemon --exec ./node_modules/.bin/ts-node -- "./src/main.ts" && pm2 startOrRestart ecosystem.config.js'
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
 }
