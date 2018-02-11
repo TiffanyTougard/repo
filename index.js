dbb0befdfdd160e8708d47db54ae2f129fe5ed01
Tiffany Tougard <snooopygirl@gmail.com>
2018-02-11 15:48:14 +0100

    Setup Finish

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(function(req, res, next){
         return next();
     }
 
-    fs.createReadStream(staticRoot + './dist/index.html').pipe(res);
+    fs.createReadStream(staticRoot + 'dist/index.html').pipe(res);
 
 }); 
 
